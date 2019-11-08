cd2f19e4b972b60c79798ff93714db9c52fc1279
Xu <xushengquan@souche.com>
2019-11-08 17:17:50 +0800

    setting

--- Chatbot_Retrieval/settings.py
+++ Chatbot_Retrieval/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1f(%a_ai!ey%nuj00$h9^!_zu2&8n^5ekb--uvfzb2c19h7kj4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
